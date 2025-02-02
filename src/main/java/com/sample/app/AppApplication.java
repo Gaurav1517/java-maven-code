a1062fad573aaac68f5493306cb89071fc1f0968
Devloper <devloper01@gmail.com>
2025-02-02 17:12:46 +0530

    adding source code

---
+++ src/main/java/com/sample/app/AppApplication.java
@@ -0,0 +1,30 @@
+// package com.sample.app;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class AppApplication {
+
+//     public static void main(String[] args) {
+//         System.out.println("Hello"); // Print "Hello" to the console
+//         SpringApplication.run(AppApplication.class, args);
+//     }
+// }
+
+
+
+
+package com.sample.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello"); // Print "Hello" to the console
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
